cb2f414df1aadf20020ead1219e28ff16a223f46
KseniiaMazan <ksusamazan@gmail.com>
2019-09-12 17:37:10 +0300

    Updates

---
+++ precache-manifest.412d9dbe67b5df8816294c0c551d76d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d515428f214fc1696543fce5070fe323",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "314d9ecbe75446102a06",
+    "url": "/react_tabs/static/css/main.28667f98.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react_tabs/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "314d9ecbe75446102a06",
+    "url": "/react_tabs/static/js/main.b9557fcc.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
